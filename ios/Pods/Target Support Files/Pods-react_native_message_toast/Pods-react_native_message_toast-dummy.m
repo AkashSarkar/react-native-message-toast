273e64fe86dbefc256df5ae346c60048e0258e91
Akash Sarkar <akashsarker1433@gmail.com>
2021-05-07 23:21:08 +0600

    v-0.1.1

--- ios/Pods/Target Support Files/Pods-react_native_message_toast/Pods-react_native_message_toast-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_react_native_message_toast : NSObject
-@end
-@implementation PodsDummy_Pods_react_native_message_toast
-@end
--- ios/test.swift
+++
@@ -1,8 +0,0 @@
-//
-//  test.swift
-//  react_native_message_toast
-//
-//  Created by Akash Sarkar on 7/5/21.
-//
-
-import Foundation
